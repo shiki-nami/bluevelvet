138f96c744fd0793a8dcc153c8bfc359ed8ccf45
shiki-nami <239708491+shiki-nami@users.noreply.github.com>
2026-01-23 21:55:46 +0800

    fix

--- files/scripts/clash-party.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-dnf install -y $(curl -s https://api.github.com/repos/mihomo-party-org/clash-party/releases/latest | grep -o 'https://[^"]*clash-party-linux-[^"]*-x86_64\.rpm' | head -n1)
