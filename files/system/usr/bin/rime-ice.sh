072262d7d757a5d51f55db2b5bf9ee983fbd8297
shiki-nami <239708491+shiki-nami@users.noreply.github.com>
2026-04-22 03:04:54 +0800

    添加雾凇拼音

---
+++ files/system/usr/bin/rime-ice.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TARGET_DIR="$HOME/.config/ibus/rime"
+
+mkdir -p "$TARGET_DIR"
+cd "$TARGET_DIR"
+
+# 已有 git 仓库则拉取
+if [ -d ".git" ]; then
+    git pull
+else
+    # 否则克隆
+    git clone --depth 1 https://github.com/iDvel/rime-ice .
+fi
